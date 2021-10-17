4d4dfd039d4d9a604faa6d9ff070ca3f434d5b88
Tara Zadlyka <tara02zadlyka@gmail.com>
2021-10-17 18:08:55 +0700

    upload

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+    $(".filter-button").click(function() {
+        var value = $(this).attr('data-filter');
+
+        if (value == "all") {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        } else {
+            //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+            //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.' + value).hide('3000');
+            $('.filter').filter('.' + value).show('3000');
+
+        }
+    });
+
+    if ($(".filter-button").removeClass("active")) {
+        $(this).removeClass("active");
+    }
+    $(this).addClass("active");
+
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+    var myOffcanvas = document.getElementById('offcanvasRight');
+    var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+    document.getElementById("OpenMenu").addEventListener('click',function (e){
+      e.preventDefault();
+      e.stopPropagation();
+      bsOffcanvas.toggle();
+    });
+  });
